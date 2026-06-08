f4160eea55a6c845d140ce5bedd7717029d05ed0
Urieel <urielsalazar.dev@gmail.com>
2026-06-08 12:16:20 -0600

    Prepare for railway adding docker file

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import home_view,about_view
 urlpatterns = [
+    path('about/',about_view),
     path('admin/', admin.site.urls),
+    path('hello-world.html',home_view),
+    path('hello-world/',home_view)
 ]
