91bbfa58f75d7f725cb12a7081ff4ee7823bb79a
Urieel <urielsalazar.dev@gmail.com>
2026-06-08 13:53:31 -0600

    Add python deocuple

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from dotenv import load_dotenv
 import os 
+from decouple import config
 from pathlib import Path
 
 load_dotenv() # load environment variables 
@@ -23,10 +24,10 @@ print('BASE_DIR',BASE_DIR)
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     ".railway.app"
