30a3ae856617f1d546512e259f6ebc0e932d57d1
Urieel <urielsalazar.dev@gmail.com>
2026-06-09 13:17:37 -0600

    Refactor database configuration to use DATABASE_URL and remove default SQLite setup

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -86,15 +86,10 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast = int, default = 30)
-DATABASE_URL = config('DATABASE_URL', cast = str)
+DATABASE_URL = config('DATABASE_URL', cast = str,default=None)
 
 if DATABASE_URL is not None:
     DATABASES = {
