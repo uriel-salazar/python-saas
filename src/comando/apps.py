8478bab41f947ddd69cb780c24035fe4f3c2b587
Urieel <urielsalazar.dev@gmail.com>
2026-06-11 09:10:11 -0600

    Create new called comando

---
+++ src/comando/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ComandoConfig(AppConfig):
+    name = 'comando'
---
+++ src/comando/commands/hello_word.py
@@ -0,0 +1,8 @@
+from typing import Any
+
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    
+    def handle(self, *args: Any, **options: Any) :
+       print("hello word")
