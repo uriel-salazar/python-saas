a2a59fbe6e20fee4b39220ea74fe087cb54fdb3d
Urieel <urielsalazar.dev@gmail.com>
2026-06-10 18:19:57 -0600

    Update .gitignore and settings.py for static files configuration

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -133,4 +133,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+
+# source for python manage.py collecstatic
+
+STATICFILES_DIRS = [
+        STATICFILES_BASE_DIR
+    ]
+
+# output for python manage.py collecstatic
+
+# local cdn 
+STATIC_ROOT = BASE_DIR.parent / " local-cdn "
