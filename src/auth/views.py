f3e1610a81c3a5c524bb8f908df5e4d7fdfcaee8
Urieel <urielsalazar.dev@gmail.com>
2026-06-24 12:36:14 -0600

    Add register url to try with form

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 def login_view(request):
     error = None
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("/hello-world/")
+        username = request.POST.get("username") or None
+        password = request.POST.get("password") or None
+        if all([username,password]):
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # redirects to main page 
+                return redirect("/hello-world/")
+        # otherwhise, it returns an error 
         error = "Invalid username or password."
     return render(request, "auth/login.html", {"error": error})
+
+
+def register_view(request):
+   # error=None
+ #   if request.method=='POST':
+  #     username=request.POST.get("username") or None
+   #    email=request.POST.get("email") or None
+   #    password=request.POST.get("password") or None
+    if request.method == 'POST':
+        print(request.POST)
+        username = request.POST.get("username") or None
+        email = request.POST.get("email") or None
+        password = request.POST.get("password") or None
+     #   username_exists= User.objects.filter(username___iexact = username).exists()
+      #  email_exists= User.objects.filter(email___iexact = email).exists()
+        try:
+            User.objects.create_user(username,email=email,password=password)
+        except:
+            pass
+    return render(request, "auth/register.html", {})
+    
+
+    
