aa53df967d995833832df97ec575d9cc14005a02
Urieel <urielsalazar.dev@gmail.com>
2026-06-27 12:43:03 -0600

    Try simple sessions

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from auth import views as auth_views
-from .views import home_view,about_view
+from .views import home_view,about_view,pw_protected_view
 urlpatterns = [
     path('',home_view,name='home'),
     path('about/',about_view),
@@ -26,6 +26,7 @@ urlpatterns = [
     path('hello-world/',home_view),
     path('register/',auth_views.register_view),
     path('accounts/', include('allauth.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('protected/',pw_protected_view)
 
 ]
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -28,3 +28,16 @@ def about_view(request,*args,**kwargs):
     PageVisit.objects.create(path=request.path)
     
     return render(request,html_template,my_context)
+
+VALID_CODE = 'abc123'
+def pw_protected_view(request,*args,**kwargs):
+    is_allowed = request.session.get('protected_page_allowed') or 0 
+    if request.method =='POST':
+        user_pw_request = request.POST.get("code") or None
+        if user_pw_request == VALID_CODE:
+            is_allowed=1
+            request.session['protected_page_allowed'] = is_allowed
+    if is_allowed:
+            return render(request,'protected/view.html',{})
+    
+    return render(request,'protected/entry.html',{})
