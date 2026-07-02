90b889f046f0f3b8dbd8c9103e80043c6ca23109
Urieel <urielsalazar.dev@gmail.com>
2026-07-02 08:39:05 -0600

    Sync Users with their respect user group

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -76,7 +76,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "allauth.socialaccount.providers.github",
     "widget_tweaks",
-    "slippers"
+    
     
 ]
 
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,14 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import Group,Permission
+from django.conf import settings 
 
-class SubscriptionModels(models.Model):
-    name=models.CharField(max_length=100)
 
-    class Meta:
-        # subscriptions permissions 
-        permissions=[
+SUBCRIPTION_PERMISSIONS =  [
             ("advanced","Advanced Perm"),
             ("pro","Pro Perm"),
             ("basic","Basic Perm"),
             ("basic_ai","Basic AI Perm"),
 
         ]
+class Subscription(models.Model):
+    name =  models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
+    groups = models.ManyToManyField(Group)
+    
+    permissions = models.ManyToManyField(Permission,
+    limit_choices_to = {
+        "content_type__app_label":"subscriptions",
+        "codename__in":[x[0] for x in SUBCRIPTION_PERMISSIONS
+        ]
+        }
+    )
+    class Meta:
+        # subscriptions permissions 
+        permissions = SUBCRIPTION_PERMISSIONS
