d72ea57e2b03204e0c335b773998466ad7009b6d
Urieel <urielsalazar.dev@gmail.com>
2026-07-23 11:08:22 -0600

    Add pricing page and update navbar for subscription pricing link

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -19,19 +19,22 @@ from django.urls import path,include
 from auth import views as auth_views
 from .views import(home_view,about_view,pw_protected_view,login_required,user_only_view,
                    staff_only_view)
+from subscriptions import views as subscriptions_view
 
 urlpatterns = [
     path('',home_view,name='home'),
+    path('pricing/',subscriptions_view.subscriptions_price_view,name='pricing'),
     path('about/',about_view),
-    path('login/',auth_views.login_view),
+  # path('login/',auth_views.login_view),
+    #  path('register/',auth_views.register_view)
     path('hello-world.html',home_view),
     path('hello-world/',home_view),
-    path('register/',auth_views.register_view),
     path('accounts/', include('allauth.urls')),
     path('protected/',pw_protected_view),
     path('protected/user-only',user_only_view),
     path('protected/staff-only',staff_only_view),
     path('profiles/',include('profiles.urls')),
+  
     path('admin/', admin.site.urls),
 
 ]
