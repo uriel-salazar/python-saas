6a68a9d0791622daf699f0f02ea87c07a58e4f8b
Urieel <urielsalazar.dev@gmail.com>
2026-07-23 09:53:31 -0600

    Add subscription and pricing models with Stripe integration

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -31,3 +31,31 @@ def create_customer(name = '',email = '',metadata = {},raw=False):
     stripe_id=response.id
     return stripe_id
 
+def create_product(name = '',email = '',metadata = {},raw=False):
+    response = stripe.Product.create(name = name,
+                                      metadata=metadata)
+    if raw:
+        return response
+    stripe_id=response.id
+    return stripe_id
+
+def create_price(currency = "usd",
+                unit_amount = 9999,
+                interval = "month",
+                product = None,
+                metadata = {},
+                raw = False,
+                ):
+        if product is None:
+            return None 
+        response =  stripe.Price.create(
+            currency = currency,
+            unit_amount = unit_amount,
+            recurring = {"interval": interval},
+            product = product,
+            metadata = metadata 
+        )
+        if raw:
+            return response
+        stripe_id=response.id
+        return stripe_id
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,3 +1,4 @@
+import helpers.billing 
 from django.db import models
 from django.contrib.auth.models import Group,Permission
 from django.conf import settings 
@@ -12,6 +13,7 @@ SUBCRIPTION_PERMISSIONS = [
         ("basic_ai","Basic AI Perm"),
     ]
 class Subscription(models.Model):
+    """ Subscription Plan Product """
     name =  models.CharField(max_length=120)
     active = models.BooleanField(default=True)
     groups = models.ManyToManyField(Group)
@@ -23,13 +25,98 @@ class Subscription(models.Model):
         ]
         }
     )
+    stripe_id = models.CharField(max_length =  120, null = True, blank = True)
+
+    order = models.IntegerField(default = -1 , help_text = "Ordering on Django pricing page")
+    featured = models.BooleanField(default = True ,help_text= """Feature on Django 
+    pricing page""" )
+    updated= models.DateTimeField(auto_now = True)
+    timestamp = models.DateTimeField(auto_now_add = True)
     
     def __str__(self):
         return self.name 
     class Meta:
-        # subscriptions permissions 
+        ordering = ['order','featured','-updated']
         permissions = SUBCRIPTION_PERMISSIONS
+    
+    def save(self, *args, **kwargs):
         
+        if not self.stripe_id:
+                stripe_id = helpers.billing.create_product(name = self.name,
+                metadata = {"subscription_plan_id":self.id},
+                     raw = False) 
+                self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
+        
+        
+
+class SubscriptionPrice(models.Model):
+    """ Subscription Price  = Stripe Price """
+    class IntervalChoices(models.TextChoices):
+        """
+        The first value is what it gets stored in db, the seccond one is the django view.
+        The user has two choices to choose from, monthly or yearly. 
+        """
+        MONTHLY = "month","Monthly"
+        YEARLY = "year","Yearly"
+
+    subscription = models.ForeignKey(Subscription, on_delete = models.SET_NULL,
+    blank = True, null = True)
+    stripe_id = models.CharField(max_length =  120, null = True, blank = True)
+    interval = models.CharField(max_length = 120, default = IntervalChoices.MONTHLY,
+                                choices = IntervalChoices.choices)
+    price= models.DecimalField(max_digits =  10,decimal_places =  2 , default = 99.99)
+    order = models.IntegerField(default = -1 , help_text = "Ordering on Django pricing page")
+    featured = models.BooleanField(default = True ,help_text= """Feature on Django 
+    pricing page""" )
+    updated= models.DateTimeField(auto_now = True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    class Meta:
+        ordering = ["subscription__order","order","featured", "-updated"]
+        
+    @property
+    def stripe_currency(self):
+        return 'usd' 
+    
+    @property
+    def stripe_price(self):
+        
+        """
+        remove decimal places  
+        """
+        return int(self.price * 100 )
+    
+    @property
+    def product_stripe_id(self):
+        if not self.subscription:
+            return None
+        return self.subscription.stripe_id
+    
+    def save(self,*args, **kwargs):
+        if ( not self.stripe_id and 
+            self.product_stripe_id is not None):
+        
+        
+            stripe_id = helpers.billing.create_price(
+                currency = self.stripe_currency,
+                unit_amount = self.stripe_price,
+                interval = self.interval,
+                product = self.product_stripe_id,
+                metadata = {
+                    "subscription_plan_price_id": self.id
+                },
+                raw=False 
+            )
+            self.stripe_id = stripe_id
+        super().save(*args,**kwargs)
+    
+        if self.featured and self.subscription :
+            qs = SubscriptionPrice.objects.filter(
+                subscription = self.subscription,
+                interval = self.interval
+                ).exclude(id = self.id)
+            qs.update(featured = False)
         
 class UserSubscription(models.Model):
     # If the user is deleted the subscription will be deleted as well
