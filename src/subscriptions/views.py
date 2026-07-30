94b995370dc2415dcfa93db8ac46a273dbc58628
Urieel <urielsalazar.dev@gmail.com>
2026-07-30 16:57:16 -0600

    Refactor subscriptions_price_view to accept interval parameter and enhance pricing.html layout with additional content

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from subscriptions.models import SubscriptionPrice
 
-def subscriptions_price_view(request):
+def subscriptions_price_view(request,interval = "month"):
+    qs  = SubscriptionPrice.objects.filter(featured = True)
     monthly_qs = SubscriptionPrice.objects.filter(
         interval = SubscriptionPrice.IntervalChoices.MONTHLY,
     )
     yearly_qs = SubscriptionPrice.objects.filter(
         interval = SubscriptionPrice.IntervalChoices.YEARLY,
     )
+    object_list = interval = SubscriptionPrice.IntervalChoices.MONTHLY
+    
     return render(request,"subscriptions/pricing.html",{
         "monthly_qs": monthly_qs,
         "yearly_qs": yearly_qs
