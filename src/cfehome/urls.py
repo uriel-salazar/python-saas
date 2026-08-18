60a7349e48c3d8b74625aa6519a5354a53793df0
uriel-salazar <urielsalazar.dev@gmail.com>
2026-08-18 15:25:20 -0600

    Redirect select plan to log in or signup if the user doesn't has an account

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -19,10 +19,25 @@ from django.urls import path,include
 from auth import views as auth_views
 from .views import(home_view,about_view,pw_protected_view,login_required,user_only_view,
                    staff_only_view)
+from checkouts  import views as checkout_views
 from subscriptions import views as subscriptions_view
 
 urlpatterns = [
     path('',home_view,name='home'),
+    path('checkout/sub-price/<int:price_id>/' , 
+         checkout_views.product_price_redirect_view,
+         name = 'sub-price-checkout'
+         ),
+    path('checkout/start/',
+         checkout_views.checkout_redirect_view ,
+         name = 'stripe-checkout-start',
+         
+         ),
+    
+    path('checkout/sucess/',
+             checkout_views.checkout_finalize_view,
+             name='stripe-checkout-finalize',
+             ),
     path('pricing/',subscriptions_view.subscriptions_price_view,name='pricing'),
     path('pricing/<str:interval>/',subscriptions_view.subscriptions_price_view,name = 'pricing_interval'),
     path('about/',about_view),
